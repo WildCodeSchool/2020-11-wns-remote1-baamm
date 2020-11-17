c3ba06e59cc19ed13ffa506d206fd2112b1d2649
Mayana <mayanabastard@gmail.com>
2020-11-17 15:15:13 +0100

    add photo + move data/users.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import photo from './pictures/photo.jpeg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={photo} className="App-logo" alt="photo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
