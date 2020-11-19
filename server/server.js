deb4d17caa4f663eb32bd7ad78b637c6fa60a139
Lucci Anthony <totowdu80@gmail.com>
2020-11-19 09:48:00 +0100

    Editing server.js to add chatroom in backend

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const cors = require('cors');
-const port = process.env.PORT || 5000;
 const index = require("./routes/index");
 const data = require('./data/users');
+
+const PORT = 5000;
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
 
 // const client = require("../client/src/App")
 
@@ -31,6 +33,23 @@ io.on("connection", (socket) => {
   });
 });
 
+io.on("connection", (socket) => {
+  
+  // Join a conversation
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  // Leave the room if the user closes the socket
+  socket.on("disconnect", () => {
+    socket.leave(roomId);
+  });
+});
+
 const getApiAndEmit = socket => {
   const response = data();
   console.log("DATA ::: ", response);
@@ -39,4 +58,6 @@ const getApiAndEmit = socket => {
   socket.emit("FromAPI", response);
 };
 
-server.listen(port,() => console.log(`Cors allowed. Server started on port ${port} : http://localhost:5000`));
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
