dc92fe979433eb4b69af45a83a61fc6f3fab1350
Lucci Anthony <totowdu80@gmail.com>
2020-11-20 11:43:05 +0100

    Merge conflict solve

--- server/data/askTalking.js
+++ server/data/askTalking.js
@@ -1,10 +1,30 @@
-import './users';
+function User(id, alias, lastname, firstName, role, askTalking) {
+    this.id = id,
+        this.alias = alias,
+        this.lastname = lastname,
+        this.firstName = firstName,
+        this.role = role};
+
+const role = {
+    STUDENT: 'student',
+    TEACHER: 'teacher'
+}
+
+const anthony = new User(1, 'Anthony', 'Lucci', 'Anthony', role.STUDENT);
+const aurelien = new User(2, 'Aurélien', 'Landouer', 'Aurélien', role.STUDENT);
+const brian = new User(3, 'Brian', 'Lagaude', 'Brian', role.STUDENT);
+const mansour = new User(4, 'Mansour', 'Mahamat-Salle', 'Mansour');
+const mayana = new User(5, 'Mayana', 'Bastard', 'Mayana', role.STUDENT);
+const thomas = new User(6, 'Thomas', 'Culdaut', 'Thomas', role.TEACHER);
+
+
+
 
 function AskTalking(id, user, interventionType, askingDate) {
     this.id = id,
-    this.user = user,
-    this.interventionType = interventionType,
-    this.askingDate  = askingDate
+        this.user = user,
+        this.interventionType = interventionType,
+        this.askingDate = askingDate
 };
 
 const interventionType = {
@@ -12,21 +32,18 @@ const interventionType = {
     INFORMATION: 'information'
 };
 
-// let dateTimeNow = new Date(hours, minutes, seconds);
-
-//Date Fixtures:
-// let dateTime1 = new Date("November 18, 2020 10:50:00");
-// let dateTime2 = new Date("November 18 , 2020 11:55:00");
-
-//Get minutes/secondes
-//let getMinutes = dateTimeNow.getMinutes();
-//let getSecondes = dateTimeNow.getSeconds();
-//let getTime = getMinutes + getSecondes;
 
 
-let notif1  = new AskTalking(1, anthony,  interventionType.QUESTION,     new DateTime());
-let notif2  = new AskTalking(2, aurelien, interventionType.INFORMATION,  new DateTime());
-let notif3  = new AskTalking(3, brian,    interventionType.QUESTION,     new DateTime());
+function CreateAskTalkings() {
 
-module.exports = AskTalking;
+    let notif1 = new AskTalking(1, anthony, interventionType.QUESTION, new Date());
+    let notif2 = new AskTalking(2, aurelien, interventionType.INFORMATION, new Date());
+    let notif3 = new AskTalking(3, brian, interventionType.QUESTION, new Date());
+
+    return [notif1, notif2, notif3];
+}
+
+
+
+module.exports = CreateAskTalkings;
 
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const socketIo = require("socket.io");
 const cors = require('cors');
 
 const index = require("./routes/index");
-const data = require('./data/users');
+const data = require('./data/askTalking');
 
 const PORT = 5000;
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
@@ -17,25 +17,65 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = socketIo(server);
-
 let interval;
 
+let askingTalkArray = data();
+/////////////////////////////////////
+////////// SEBASTIEN ////////////////
+////////////////////////////////////
+let clients = [];
+
 io.on("connection", (socket) => {
+  clients.push(socket);
   console.log("New client connected");
   socket.emit("FromAPI")
-  *// if (interval) {
-  //   clearInterval(interval);
-  // }
-  // interval = setInterval(() => getApiAndEmit(socket), 1000);
-  getApiAndEmit(socket);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
-  });
+    *// if (interval) {
+    //   clearInterval(interval);
+    // }
+    // interval = setInterval(() => getApiAndEmit(socket), 1000);
+    getApiAndEmit(socket);
   
+
   // Join a conversation
   const { roomId } = socket.handshake.query;
   socket.join(roomId);
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  // PARTIE ASKINGTALK
+  socket.on('askingtalk from client', (askingtalk) => {
+    console.log("askingtalk from client ::: ", askingtalk);
+    askingTalkArray.push(askingtalk);
+    console.log("ICI LE NOUVEL ASKING TALK DU SERVER ::: ", askingTalkArray);
+    clients.forEach(client => {
+      client.emit('askingtalk from server', askingTalkArray);  
+    });
+    //socket.emit('askingtalk from server', askingTalkArray);
+  });
+  socket.on('cancel askingtalk', (askingtalkid) => {
+    console.log("Annulation de prise de parole depuis le client - id ::: ", askingTalkArray);
+    askingTalkArray = askingTalkArray.filter((askingtalk) => {
+      return askingtalk.id !== askingtalkid;
+    });
+    console.log("NOUVEAU TABLEAU APRES SUPPRESSION ::: ", askingTalkArray);
+    clients.forEach(client => {
+      client.emit('askingtalk deleted', askingTalkArray);  
+    });
+    // TODO : récupérer l'askingtalk et la supprimer
+  })
+  // FIN PARTIE ASKINGTALK
+
+
+  // ATTENTION fusion de deux comportements !!!
+  // Leave the room if the user closes the socket
+  socket.on("disconnect", () => {
+    socket.leave(roomId);
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 
   // Listen for new messages
   socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
@@ -50,8 +90,8 @@ io.on("connection", (socket) => {
 
 const getApiAndEmit = socket => {
   const response = data();
-  console.log("DATA ::: ", response);
-  
+  // console.log("DATA ::: ", response);
+
   // Emitting a new message. Will be consumed by the client
   socket.emit("FromAPI", response);
 };
