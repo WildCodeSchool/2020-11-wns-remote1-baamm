630afda5db20300fce36ebd53e1de9aaf84f95f6
Mayana <mayanabastard@gmail.com>
2020-12-16 12:18:03 +0100

    dirty commit

--- client/src/ClientComponent.tsx
+++ client/src/ClientComponent.tsx
@@ -5,27 +5,27 @@ const ENDPOINT = "http://localhost:5000";
 
 const ClientComponent = () => {
   
-  return (
-    <div>
-      <header className="App-header">
-        <ul>
-          {response.map((user) => (
+  // return (
+  //   <div>
+  //     <header className="App-header">
+  //       <ul>
+  //         {response.map((user) => (
 
-            // <li key={user.id}>{user.user} <br />{user.interventionType} <br />{user.askingDate} {dateDiff(Date.now, user.askingDate)} <br />  </li>
-            <li key={user.id}>{user.user}<br />{user.interventionType}<br />
+  //           // <li key={user.id}>{user.user} <br />{user.interventionType} <br />{user.askingDate} {dateDiff(Date.now, user.askingDate)} <br />  </li>
+  //           <li key={user.id}>{user.user}<br />{user.interventionType}<br />
             
-              {/* {dateDiff((user.askingDate))}  */}
-              {console.log(dateDiff(new Date(user.askingDate)))}
+  //             {/* {dateDiff((user.askingDate))}  */}
+  //             {console.log(dateDiff(new Date(user.askingDate)))}
 
 
-            </li>
+  //           </li>
 
-          ))}
+  //         ))}
 
-        </ul>
-      </header>
-    </div>
-  );
+  //       </ul>
+  //     </header>
+  //   </div>
+  // );
 
 
 
--- client/src/components/AskTalkingButton.tsx
+++ client/src/components/AskTalkingButton.tsx
@@ -7,17 +7,25 @@ const socket = socketIOClient(ENDPOINT, {
     transports: ['websocket']
 });
 
+type AskingTalk = {
+    id: number,
+    user: object,
+    interventionType: string,
+    askingDate: Date
+};
+
 export default function AskTalkingButton() {
 
+    
 
     // correspond à l'objet askingTalk qui sera la demande de prise de parole (avec l'utilisateur, le type d'intervention et la date de demande)
-    const [askingTalk, setAskingTalk] = React.useState(null);
+    const [askingTalk, setAskingTalk] = React.useState<AskingTalk | null>(null);
     // pour stocker l'id d'un askingTalk à supprimer
-    const [askingTalkId, setAskingTalkId] = React.useState(null);
+    const [askingTalkId, setAskingTalkId] = React.useState<number |null>(null);
     // correspond à l'utilisateur connecté -- écrit en dur pour le moment
     const thisUser =
     {
-        id: Math.floor(Math.random() * 101),
+        id: Math.floor(Math.random() * 100) + 1,
         alias: 'Youpi',
         lastName: 'Célafête',
         firstName: 'Youpi',
@@ -25,16 +33,16 @@ export default function AskTalkingButton() {
         askTalking: askingTalk
     }
 
-    useEffect(() => {
-        // A chaque fois qu'on reçoit un asktalking depuis le serveur
-        socket.on('askingtalk from server', askingTalkArray => {
-            console.log("Réception d'un nouvel askingTalkArray depuis le serveur ::: ", askingTalkArray);
-        });
-        // A chaque fois qu'on supprime un asktalking depuis le serveur
-        socket.on('askingtalk deleted', askingTalkArray => {
-            console.log("Suppression d'un askingTalk depuis le serveur - new asking talk array ::: ", askingTalkArray);
-        });
-    }, []);
+    // useEffect(() => {
+    //     // A chaque fois qu'on reçoit un asktalking depuis le serveur
+    //     socket.on('askingtalk from server', askingTalkArray => {
+    //         console.log("Réception d'un nouvel askingTalkArray depuis le serveur ::: ", askingTalkArray);
+    //     });
+    //     // A chaque fois qu'on supprime un asktalking depuis le serveur
+    //     socket.on('askingtalk deleted', askingTalkArray => {
+    //         console.log("Suppression d'un askingTalk depuis le serveur - new asking talk array ::: ", askingTalkArray);
+    //     });
+    // }, []);
 
     // déclenché par les changements sur AskingTalk, donc dans les fonctions sendAskTalking et cancelAskTalking appelées par le bouton
     useEffect(() => {
@@ -50,7 +58,7 @@ export default function AskTalkingButton() {
 
 
     // fonction appelée par le clic sur le bouton quand on n'a pas encore demandé la parole
-    const sendAskTalking = (e) => {
+    const sendAskTalking = () => {
         // seul les élèves ont une raison de demander la parole 
         // TODO : retirer ce if et n'afficher le bouton que si l'user est un élève
         if (thisUser.role !== 'student') {
@@ -64,7 +72,7 @@ export default function AskTalkingButton() {
         // on "crée" notre demande de parole (plus tard : gérer le type d'intervention)
         setAskingTalk(
             {
-                id: Math.floor(Math.random() * 101),
+                id: Math.floor(Math.random() * 100) + 1,
                 user: thisUser,
                 interventionType: choosenInterventionType,
                 askingDate: new Date()
@@ -73,24 +81,29 @@ export default function AskTalkingButton() {
     }
 
     // fonction appelée par le clic sur le bouton quand on a déjà demandé la parole
-    const cancelAskTalking = (e) => {
+    const cancelAskTalking = () => {
         let sureToCancel = window.confirm(`Etes-vous sûr(e) de vouloir annuler votre demande d'intervention ?
         Vous perdrez votre place dans la file d'attente...`);
         if (sureToCancel) {
             setAskingTalkId(askingTalk.id);
-            setAskingTalk(null);
+            setAskingTalk({
+                id: 0,
+                user: {},
+                interventionType: '',
+                askingDate: new Date()
+            });
         }
  
     }
 
     return (
         <div>
-            { askingTalk &&
-                <button onClick={(e) => { cancelAskTalking(e) }} className="cancelAskTalking">Don't need to blabla anymore</button>
+            { askingTalk.id !== 0 &&
+                <button onClick={() => { cancelAskTalking() }} className="cancelAskTalking">Don't need to blabla anymore</button>
             }
 
-            { askingTalk === null &&
-                <button onClick={(e) => { sendAskTalking(e) }} className="askTalking">I want to blabla</button>
+            { askingTalk.id === 0 &&
+                <button onClick={() => { sendAskTalking() }} className="askTalking">I want to blabla</button>
             }
         </div>
     )
