a0432f62e8866b007be327913c08ace2a21065e5
Brian Lagaude <brian.lagaude@gmail.com>
2020-12-17 15:54:33 +0100

    add test folder

---
+++ client/src/test/Test.tsx
@@ -0,0 +1,9 @@
+function Test(argu: string) {
+  const getParameter = () => (
+    `on dit ${argu}`
+  );
+
+  return getParameter();
+}
+
+export default Test;
---
+++ client/src/test/index.spec.ts
@@ -0,0 +1,10 @@
+// import Test from './Test';
+
+// Test
+/*
+describe('Pain au chocolat ou chocolatine', () => {
+  it('return Pain au chocolat', () => {
+    expect(Test('Pain au chocolat')).toEqual('on dit Pain au chocolat');
+  });
+});
+*/
