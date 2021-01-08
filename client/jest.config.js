018c7d8c0ff986329105297a5cc2dae9e63c9b75
Mayana <mayanabastard@gmail.com>
2021-01-08 11:23:42 +0100

    ci tests should work

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   modulePathIgnorePatterns: ['<rootDir>/client'],
   globals: {
     'ts-jest': {
--- client/src/test/participants.spec.tsx
+++ client/src/test/participants.spec.tsx
@@ -5,24 +5,24 @@ import Participants from '../components/Participants';
 
 // Test
 
-describe('Pain au chocolat ou chocolatine', () => {
-  it('return Pain au chocolat', () => {
-    expect(Test('Pain au chocolat')).toEqual('on dit Pain au chocolat');
+describe('Pain au chocolat ou Chocolatine', () => {
+  it('return Chocolatine', () => {
+    expect(Test('Chocolatine')).toEqual('on dit Chocolatine');
   });
 });
 
-// describe('Participant Name', () => {
-//   it('render one participant name', () => {
-//     render(<Participants />);
-//     expect(screen.getByText('Virgile Rietsch' && 'Mayana Bastard'));
-//     // const allH3 = screen.getAllByRole('heading', { level: 3 })[0];
-//     // expect(allH3).toEqual(<h3>Virgile Rietsch </h3>);
-//   });
-// });
+describe('Participant Name', () => {
+  it('render one participant name', () => {
+    render(<Participants />);
+    expect(screen.getByText('Virgile Rietsch' && 'Mayana Bastard'));
+    // const allH3 = screen.getAllByRole('heading', { level: 3 })[0];
+    // expect(allH3).toEqual(<h3>Virgile Rietsch </h3>);
+  });
+});
 
-// describe('Participants list', () => {
-//   it('render the 5 participants', () => {
-//     render(<Participants />);
-//     expect(screen.getAllByRole('heading', { level: 3 })).toHaveLength(5);
-//   });
-// });
+describe('Participants list', () => {
+  it('render the 5 participants', () => {
+    render(<Participants />);
+    expect(screen.getAllByRole('heading', { level: 3 })).toHaveLength(5);
+  });
+});
