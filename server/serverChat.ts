9dc3090447da626065f43909424721e2b403405b
Mayana <mayanabastard@gmail.com>
2021-02-03 12:02:22 +0100

    clean serverChat.ts

--- server/serverChat.ts
+++ server/serverChat.ts
@@ -1,6 +1,5 @@
-import express,{ Request, Response, NextFunction } from 'express';
+import express from 'express';
 import http from 'http';
-import socketIo from 'socket.io';
 import cors from 'cors';
 
 const PORT: number = 5000;
@@ -9,12 +8,10 @@ const NEW_CHAT_MESSAGE_EVENT: string = "newChatMessage";
 const app = express();
 app.use(cors());
 
-// const io = socketIo(server);
 const socketio = require('socket.io');
 const server = http.createServer(app);
 
 socketio.on("connection", (socket: any) => {
-// io.on("connection", (socket: any) => {
   
   // Join a conversation
   const { roomId } = socket.handshake.query;
@@ -22,7 +19,6 @@ socketio.on("connection", (socket: any) => {
 
   // Listen for new messages
   socket.on(NEW_CHAT_MESSAGE_EVENT, (data:any) => {
-    // io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
      socket.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
   });
 
