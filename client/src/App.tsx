84cc1545a89be3318f9c2be6e962efb95d805d65
Mayana <mayanabastard@gmail.com>
2021-04-06 15:20:24 +0200

    fix messages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-
+import ChatContext from './context/ChatContext';
 import './App.css';
 import Teacher from './components/Teacher/Teacher';
 import DynamicMenu from './components/DynamicMenu/DynamicMenu';
+import useChat from './components/UseChat/useChat';
 
 function App() {
+  const roomId = 'Test-Room';
+  const { messages, sendMessage } = useChat(roomId);
   return (
     <div className="App">
-      <Teacher />
-      <DynamicMenu />
+      <ChatContext.Provider value={{ messages, sendMessage }}>
+        <Teacher />
+        <DynamicMenu />
+      </ChatContext.Provider>
     </div>
   );
 }
--- client/src/components/ChatRoom/ChatRoom.tsx
+++ client/src/components/ChatRoom/ChatRoom.tsx
@@ -1,44 +1,51 @@
-import React, { useState } from 'react';
-import useChat from '../UseChat/useChat';
+import React, { useContext, useState } from 'react';
+// import useChat from '../UseChat/useChat';
 import './ChatRoom.style.css';
 import { Message } from '../../types';
+import ChatContext from '../../context/ChatContext';
 
 export default function ChatRoom() {
-  const roomId = 'Test-Room';
-  const { messages, sendMessage } = useChat(roomId);
+  const ChatContextType = useContext(ChatContext);
   const [newMessage, setNewMessage] = useState('');
 
-  const handleSendMessage = () => {
-    sendMessage(newMessage);
-    setNewMessage('');
-  };
+  if (undefined !== ChatContextType) {
+    const handleSendMessage = () => {
+      ChatContextType.sendMessage(newMessage);
+      setNewMessage('');
+    };
 
+    return (
+      <div className="toolsContainer">
+        <div className="messagesContainer">
+          <ol className="messagesList">
+            {ChatContextType.messages.map((message: Message, i: number) => (
+              <li
+                // eslint-disable-next-line react/no-array-index-key
+                key={i}
+                className={`message-item ${message.ownedByCurrentUser ? 'my-message' : 'receive-message'}`}
+              >
+                {message.body}
+              </li>
+            ))}
+          </ol>
+        </div>
+        <div className="inputBlock">
+          <textarea
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+            placeholder="Write message ..."
+            className="inputMessage"
+          />
+          <button type="button" onClick={handleSendMessage} className="sendMessage">
+            Envoyer
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="toolsContainer">
-      <div className="messagesContainer">
-        <ol className="messagesList">
-          {messages.map((message: Message, i: number) => (
-            <li
-              // eslint-disable-next-line react/no-array-index-key
-              key={i}
-              className={`message-item ${message.ownedByCurrentUser ? 'my-message' : 'receive-message'}`}
-            >
-              {message.body}
-            </li>
-          ))}
-        </ol>
-      </div>
-      <div className="inputBlock">
-        <textarea
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          placeholder="Write message ..."
-          className="inputMessage"
-        />
-        <button type="button" onClick={handleSendMessage} className="sendMessage">
-          Envoyer
-        </button>
-      </div>
+    <div>
+      <p>ERROR !!!</p>
     </div>
   );
 }
