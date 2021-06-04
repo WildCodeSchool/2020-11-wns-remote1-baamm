364c91ef764cfef05e24eeae1b51cd17f90e6de1
Totowdu80 <totowdu80@gmail.com>
2021-06-04 10:52:11 +0200

    refactor: adding react router and nav in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import ChatContext from './context/ChatContext';
 import Teacher from './components/Teacher/Teacher';
 import DynamicMenu from './components/DynamicMenu/DynamicMenu';
@@ -6,17 +12,54 @@ import useChat from './components/UseChat/useChat';
 import './App.css';
 import LoginPage from './components/LoginPage/Login';
 
+function Home() {
+  return (
+    <div className="homeContainer">
+      <p>Bienvenue sur le site des BAAMM</p>
+    </div>
+  );
+}
+
 function App() {
   const roomId = 'Test-Room';
   const { messages, sendMessage } = useChat(roomId);
   return (
-    <div className="App">
+    <Router>
       <ChatContext.Provider value={{ messages, sendMessage }}>
-        <Teacher />
-        <DynamicMenu />
+        <div>
+          <nav>
+            <ul className="navbar">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/chatroom">ChatRoom</Link>
+              </li>
+              <li>
+                <Link to="/sign">Connexion / Inscription</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/sign">
+              <LoginPage />
+            </Route>
+            <Route path="/chatroom">
+              <div className="App">
+                <Teacher />
+                <DynamicMenu />
+              </div>
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </ChatContext.Provider>
-      <LoginPage />
-    </div>
+    </Router>
   );
 }
 
