1cd0d766f8689ae6b2d0b48629b04dc964d171b3
Brian Lagaude <brian.lagaude@gmail.com>
2021-07-01 10:10:26 +0200

    add socket for video chat

--- server/server.ts
+++ server/server.ts
@@ -34,11 +34,18 @@ app.use(signinRouter);
 
 
 
-const PORT: number = 5000;
+const PORT: number = 5001;
 const NEW_CHAT_MESSAGE_EVENT: string = "newChatMessage";
 
 const httpServer = require('http').Server(app);
-const io = require('socket.io')(httpServer);
+// const io = require('socket.io')(httpServer);
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: [ "GET", "POST" ]
+  }
+})
 
 let interval: NodeJS.Timeout;
 let askingTalkArray = AskTalkings;
@@ -95,6 +102,24 @@ io.on("connection", (socket:CustomSocket) => {
     io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
   });
 
+  socket.emit("me", socket.id)
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded")
+  })
+
+  socket.on("callUser", (data: any) => {
+    io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+  })
+
+  socket.on("answerCall", (data: any) => {
+    io.to(data.to).emit("callAccepted", data.signal)
+  })  
+
+  socket.on("shareScreen", (data: any) => {
+    io.to(data.to).emit("shareScreen", data.signal)
+  })  
+  
 });
 
 httpServer.listen(PORT, () => {
