636e4f21eb55a160e46019b03f515ec81b6c0906
Totowdu80 <totowdu80@gmail.com>
2021-07-01 14:08:57 +0200

    refactor: fix problem with adress

--- client/src/services/auth.service.tsx
+++ client/src/services/auth.service.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth/';
+const API_URL = `${process.env.REACT_APP_API_URL}/api/auth/`;
 
 const login = async (email: String, password: String) => axios
   .post(`${API_URL}signin`, {
