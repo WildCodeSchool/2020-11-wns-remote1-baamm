49c76e650be116e1fb0549d63c884e69743ce5c8
Totowdu80 <totowdu80@gmail.com>
2021-07-06 14:51:37 +0200

    refactor: try fix docker

--- deploy_branch_dev.sh
+++ deploy_branch_dev.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo "PORT=$PORT"
 git fetch origin && git reset --hard origin/dev && git clean -f -dev
 GATEWAY_PORT=$PORT docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
