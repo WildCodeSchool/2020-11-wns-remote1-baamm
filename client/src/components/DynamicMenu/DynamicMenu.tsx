cf1fd9df2bc6c33d2fb39c3c35e3b43931bd6048
Totowdu80 <totowdu80@gmail.com>
2021-07-08 20:46:15 +0200

    refactor: update style for menu

--- client/src/components/DynamicMenu/DynamicMenu.tsx
+++ client/src/components/DynamicMenu/DynamicMenu.tsx
@@ -1,8 +1,4 @@
-import './DynamicMenu.style.css';
 import React, { useState } from 'react';
-import {
-  BrowserRouter as Router, Switch, Route, Link,
-} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faClock,
@@ -10,55 +6,77 @@ import {
   faUsers,
   faAngleLeft,
   faAngleRight,
-  faCog,
 } from '@fortawesome/free-solid-svg-icons';
-import Participants from '../Participants/Participants';
-import WaitingQueue from '../WaitingQueue/WaitingQueue';
-import ChatRoom from '../ChatRoom/ChatRoom';
-import '../Teacher/Teacher.style.css';
+import Participants from './Participants/Participants';
+import WaitingQueue from './WaitingQueue/WaitingQueue';
+import ChatRoom from './ChatRoom/ChatRoom';
+import './DynamicMenu.style.css';
 
 export default function DynamicMenu() {
   const [isOpen, setIsOpen] = useState(false);
-
-  function openMenu() {
-    if (isOpen === false) {
-      setIsOpen(true);
-    }
-  }
+  const [onTchat, setOnTchat] = useState(false);
+  const [onQueue, setOnQueue] = useState(false);
+  const [onUserList, setOnUserList] = useState(false);
 
   return (
-    <div className={isOpen ? 'container openedContainer' : 'container closedContainer'}>
-      <Router>
-        <div className={isOpen ? 'navToolsBar' : 'closedNavbar'}>
-          {isOpen
-            ? <FontAwesomeIcon icon={faAngleRight} className="linkTest" onClick={() => setIsOpen(false)} />
-            : <FontAwesomeIcon icon={faAngleLeft} className="linkTest" onClick={() => setIsOpen(true)} />}
-          <Link to="/chat" onClick={openMenu} className="linkTest">
-            <FontAwesomeIcon icon={faCommentAlt} />
-          </Link>
-          <Link to="/waitingqueue" onClick={openMenu} className="linkTest">
-            <FontAwesomeIcon icon={faClock} />
-          </Link>
-          <Link to="/participants" onClick={openMenu} className="linkTest">
-            <FontAwesomeIcon icon={faUsers} />
-          </Link>
-          <FontAwesomeIcon icon={faCog} className={isOpen ? 'hiddenButton' : 'linkTest optionsIcon'} />
-        </div>
-        {isOpen ? (
-          <Switch>
-            <Route path="/waitingqueue">
-              <WaitingQueue />
-            </Route>
-            <Route path="/chat">
-              <ChatRoom />
-            </Route>
-            <Route path="/participants">
-              <Participants />
-            </Route>
-          </Switch>
-        ) : null}
+    <div className={isOpen ? 'menuContainer openedContainer' : 'menuContainer closedContainer'}>
+      <div className={isOpen ? 'menuBar menuBarOpen' : 'menuBar menuBarClose'}>
+        {isOpen
+          ? (
+            <button type="button" onClick={() => setIsOpen(false)} className={isOpen ? 'buttonBarContainer buttonBarContainerOpen' : 'buttonBarContainer buttonBarContainerClose'}>
+              <FontAwesomeIcon icon={faAngleRight} className="buttonBarIcon" />
+            </button>
+          )
+          : (
+            <button type="button" onClick={() => setIsOpen(true)} className={isOpen ? 'buttonBarContainer buttonBarContainerOpen' : 'buttonBarContainer buttonBarContainerClose'}>
+              <FontAwesomeIcon icon={faAngleLeft} className="buttonBarIcon" />
+            </button>
+          )}
+        <button
+          type="button"
+          onClick={() => {
+            setOnTchat(true);
+            setOnQueue(false);
+            setOnUserList(false);
+            setIsOpen(true);
+          }}
+          className={isOpen ? 'buttonBarContainer buttonBarContainerOpen' : 'buttonBarContainer buttonBarContainerClose'}
+        >
+          <FontAwesomeIcon icon={faCommentAlt} className="buttonBarIcon" />
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setOnTchat(false);
+            setOnQueue(true);
+            setOnUserList(false);
+            setIsOpen(true);
+          }}
+          className={isOpen ? 'buttonBarContainer buttonBarContainerOpen' : 'buttonBarContainer buttonBarContainerClose'}
+        >
+          <FontAwesomeIcon icon={faClock} className="buttonBarIcon" />
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setOnTchat(false);
+            setOnQueue(false);
+            setOnUserList(true);
+            setIsOpen(true);
+          }}
+          className={isOpen ? 'buttonBarContainer buttonBarContainerOpen' : 'buttonBarContainer buttonBarContainerClose'}
+        >
+          <FontAwesomeIcon icon={faUsers} className="buttonBarIcon" />
+        </button>
+      </div>
+      {isOpen ? (
+        <>
+          {onTchat && <ChatRoom />}
+          {onQueue && <WaitingQueue />}
+          {onUserList && <Participants />}
+        </>
+      ) : null}
 
-      </Router>
     </div>
   );
 }
