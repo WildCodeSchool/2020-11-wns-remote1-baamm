03a50c6c819697df3ab7db5e70f7375e265f5f36
Brian Lagaude <brian.lagaude@gmail.com>
2021-07-15 12:25:11 +0200

    add change in video and chatroom

--- client/src/components/DynamicMenu/ChatRoom/ChatRoom.tsx
+++ client/src/components/DynamicMenu/ChatRoom/ChatRoom.tsx
@@ -1,29 +1,80 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import './ChatRoom.style.css';
+
+// export default function ChatRoom() {
+//   const [newMessage, setNewMessage] = useState('');
+//   return (
+//     <div className="toolsContainer">
+//       <div className="messagesContainer">
+//         <p>Ceci contient les messages de tchat</p>
+//         <p>Ceci contient les messages de tchat</p>
+//         <p>Ceci contient les messages de tchat</p>
+//         <p>Ceci contient les messages de tchat</p>
+//         <p>Ceci contient les messages de tchat</p>
+//         <p>Ceci contient les messages de tchat</p>
+//       </div>
+//       <div className="inputBlock">
+//         <textarea
+//           value={newMessage}
+//           onChange={(e) => setNewMessage(e.target.value)}
+//           placeholder="Write message ..."
+//           className="inputMessage"
+//         />
+//         <button type="button" onClick={() => console.log(newMessage)} className="sendMessage">
+//           Envoyer
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+import React, { useContext, useState } from 'react';
 import './ChatRoom.style.css';
+import { Message } from '../../../types';
+import ChatContext from '../../../context/ChatContext';
 
 export default function ChatRoom() {
+  const ChatContextType = useContext(ChatContext);
   const [newMessage, setNewMessage] = useState('');
+
+  if (ChatContextType) {
+    const handleSendMessage = () => {
+      ChatContextType.sendMessage(newMessage);
+      setNewMessage('');
+    };
+
+    return (
+      <div className="toolsContainer">
+        <div className="messagesContainer">
+          <ol className="messagesList">
+            {ChatContextType.messages.map((message: Message, i: number) => (
+              <li
+                // eslint-disable-next-line react/no-array-index-key
+                key={i}
+                className={`message-item ${message.ownedByCurrentUser ? 'my-message' : 'receive-message'}`}
+              >
+                {message.body}
+              </li>
+            ))}
+          </ol>
+        </div>
+        <div className="inputBlock">
+          <textarea
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+            placeholder="Write message ..."
+            className="inputMessage"
+          />
+          <button type="button" onClick={handleSendMessage} className="sendMessage">
+            Envoyer
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="toolsContainer">
-      <div className="messagesContainer">
-        <p>Ceci contient les messages de tchat</p>
-        <p>Ceci contient les messages de tchat</p>
-        <p>Ceci contient les messages de tchat</p>
-        <p>Ceci contient les messages de tchat</p>
-        <p>Ceci contient les messages de tchat</p>
-        <p>Ceci contient les messages de tchat</p>
-      </div>
-      <div className="inputBlock">
-        <textarea
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          placeholder="Write message ..."
-          className="inputMessage"
-        />
-        <button type="button" onClick={() => console.log(newMessage)} className="sendMessage">
-          Envoyer
-        </button>
-      </div>
+    <div>
+      <p>ERROR !!!</p>
     </div>
   );
 }
---
+++ client/src/components/Room/ChatVideo.tsx
@@ -0,0 +1,241 @@
+/* eslint-disable no-console */
+import React, { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Container } from '@material-ui/core';
+import Peer, { SignalData } from 'simple-peer';
+import socket from '../../socket/Socket';
+
+interface IParams {
+  id: string;
+  roomID: string;
+}
+
+interface IPayload {
+  signal: string | SignalData;
+  callerID: string;
+  id: string;
+}
+
+interface IPeerWithId {
+  peerID: string;
+  peer: Peer.Instance;
+  micro: boolean;
+  video: boolean;
+}
+
+interface IPeer {
+  peer: Peer.Instance;
+  microStatus: boolean;
+  videoStatus: boolean;
+  peerId: string;
+  videoPeerId: string;
+}
+
+const Video = ({
+  peer,
+  microStatus,
+  videoStatus,
+  peerId,
+  videoPeerId,
+}: IPeer) => {
+  const ref = useRef<HTMLVideoElement>(null);
+  const isUser = peerId === videoPeerId;
+
+  useEffect(() => {
+    if (ref?.current?.srcObject && isUser) {
+      (ref.current
+        .srcObject as MediaStream).getVideoTracks()[0].enabled = videoStatus;
+    }
+  }, [videoStatus, isUser, videoPeerId]);
+
+  useEffect(() => {
+    if (ref?.current?.srcObject && isUser) {
+      (ref.current
+        .srcObject as MediaStream).getAudioTracks()[0].enabled = microStatus;
+    }
+  }, [microStatus, isUser, videoPeerId]);
+
+  useEffect(() => {
+    peer.on('stream', (stream: MediaStream) => {
+      if (ref && ref.current) {
+        ref.current.srcObject = stream;
+      }
+    });
+  }, [peer, videoStatus]);
+
+  return (
+    <>
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+      <video
+        style={{
+          margin: '2%',
+          minHeight: '30%',
+          width: '25%',
+          borderRadius: 15,
+          borderWidth: 3,
+          borderStyle: 'solid',
+          borderColor: '#4ea55b',
+        }}
+        playsInline
+        autoPlay
+        ref={ref}
+      />
+    </>
+  );
+};
+
+const ChatVideo = () => {
+  const params = useParams<IParams>();
+  const [peerId, setPeerId] = useState<string>('');
+  const peersRef = useRef<IPeerWithId[]>([]);
+  const [peers, setPeers] = useState<Peer.Instance[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [screenShare, setScreenShare] = useState(false);
+  const [videoStatus, setVideoStatus] = useState(true);
+  const [microStatus, setMicroStatus] = useState(false);
+
+  const roomId = params.roomID;
+
+  const removeUserLeavingRoomVideo = (socketId: string) => {
+    peersRef.current = peersRef.current.filter((el) => el.peerID !== socketId);
+  };
+
+  const createPeer = (
+    userToSignal: any,
+    callerID: string,
+    stream: MediaStream,
+  ) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream,
+    });
+
+    peer.on('signal', (signal) => {
+      console.log('CREATE PEER => peer on signal');
+      socket.emit('sending signal', {
+        userToSignal,
+        callerID,
+        signal,
+      });
+    });
+    console.log('CREATE PEER => new peer', peer);
+    return peer;
+  };
+
+  const addPeer = (
+    incomingSignal: string | SignalData,
+    callerID: string,
+    stream: undefined | MediaStream,
+  ) => {
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream,
+    });
+    peer.on('signal', (signal) => {
+      console.log('ADD PEER => SIGNAL');
+
+      socket.emit('returning signal', {
+        signal,
+        callerID,
+      });
+    });
+
+    peer.signal(incomingSignal);
+
+    return peer;
+  };
+
+  useEffect(() => {
+    if (roomId) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((stream: MediaStream) => {
+          socket.emit('join room', roomId);
+
+          socket.on('all users', (users: string[]) => {
+            const usersPeers: Peer.Instance[] = [];
+            users.forEach((userID: string) => {
+              const peer = createPeer(userID, socket.id, stream);
+              if (
+                !peersRef.current.find(
+                  (peerWithId) => userID === peerWithId.peerID,
+                )
+              ) {
+                peersRef.current.push({
+                  peerID: userID,
+                  peer,
+                  micro: false,
+                  video: true,
+                });
+                setPeerId(userID);
+              }
+              usersPeers.push(peer);
+            });
+            setPeers(usersPeers);
+          });
+
+          socket.on('user joined', (payload: IPayload) => {
+            const peer = addPeer(payload.signal, payload.callerID, stream);
+            if (
+              !peersRef?.current?.find(
+                (video) => payload.callerID === video.peerID,
+              )
+            ) {
+              peersRef.current.push({
+                peerID: payload.callerID,
+                peer,
+                micro: true,
+                video: false,
+              });
+              setPeers([...peers, peer]);
+            }
+            console.log('USER JOINED', peersRef);
+            setScreenShare(!screenShare);
+          });
+
+          socket.on('receiving returned signal', (payload: IPayload) => {
+            console.log('useEffect => receiving returned signal');
+            const item = peersRef?.current?.find(
+              (p) => p.peerID === payload.id,
+            );
+            item?.peer?.signal(payload.signal);
+            console.log('RECEIVING Signal');
+          });
+
+          socket.on('removeUserVideo', (socketId: string) => {
+            console.log('useEffect => removeUserVideo');
+            removeUserLeavingRoomVideo(socketId);
+          });
+        })
+        .catch((err) => console.log('erreur dans getUserMedia : ', err));
+    }
+  }, []);
+
+  return (
+    <>
+      <Container style={{ display: 'flex', flexWrap: 'wrap' }}>
+
+        {peersRef?.current.map((peer: IPeerWithId) => (
+          <>
+            <Video
+              key={peer.peerID}
+              peer={peer.peer}
+              microStatus={microStatus}
+              videoStatus={videoStatus}
+              peerId={peerId}
+              videoPeerId={peer.peerID}
+            />
+          </>
+        ))}
+      </Container>
+      <div className="buttons">
+        <button type="button" className="button" onClick={() => setMicroStatus(!microStatus)}>micro status</button>
+        <button type="button" className="button" onClick={() => setVideoStatus(!videoStatus)}>video button</button>
+      </div>
+    </>
+  );
+};
+
+export default ChatVideo;
