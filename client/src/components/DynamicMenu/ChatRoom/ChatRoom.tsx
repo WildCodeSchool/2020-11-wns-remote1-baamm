2227d9d4df9bfbdd58e286c08d915c196eaf0cb9
Totowdu80 <totowdu80@gmail.com>
2021-07-15 16:01:11 +0200

    refactor: add name on tchat

--- client/src/components/DynamicMenu/ChatRoom/ChatRoom.tsx
+++ client/src/components/DynamicMenu/ChatRoom/ChatRoom.tsx
@@ -2,14 +2,16 @@ import React, { useContext, useState } from 'react';
 import { Message } from '../../../types';
 import ChatContext from '../../../context/ChatContext';
 import './ChatRoom.style.css';
+import AuthService from '../../../services/auth.service';
 
 export default function ChatRoom() {
   const [newMessage, setNewMessage] = useState('');
   const ChatContextType = useContext(ChatContext);
+  const currentUser = AuthService.getCurrentUser();
 
   if (ChatContextType) {
     const handleSendMessage = () => {
-      ChatContextType.sendMessage(newMessage);
+      ChatContextType.sendMessage(newMessage, currentUser);
       setNewMessage('');
     };
 
@@ -19,7 +21,7 @@ export default function ChatRoom() {
           <ol className="listMessages">
             {ChatContextType.messages.map((message: Message) => (
               <li className={message.ownedByCurrentUser ? 'message my-message' : 'message receive-message'}>
-                <p className="userName">{message.ownedByCurrentUser ? 'Moi' : message.ownedByCurrentUser}</p>
+                <p className="userName">{message.ownedByCurrentUser ? 'Moi' : `${message.userFirstname} ${message.userLastname}`}</p>
                 <p className="messageBody">{message.body}</p>
               </li>
             ))}
